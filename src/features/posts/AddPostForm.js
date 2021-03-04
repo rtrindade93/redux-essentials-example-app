c3557f08203d9cafdf814738489d11ad6b951244
Ricardo Trindade <ricardopovoas.trindade@gmail.com>
2021-03-04 17:38:14 +0000

    Users and Posts

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -1,24 +1,36 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { postAdded } from './postsSlice'
 
 const AddPostForm = () => {
   const [title, setTitle] = React.useState('')
   const [content, setContent] = React.useState('')
+  const [userId, setUserId] = React.useState('')
 
   const dispatch = useDispatch()
 
+  const users = useSelector(state => state.users)
+
   const onTitleChanged = (e) => setTitle(e.target.value)
   const onContentChanged = (e) => setContent(e.target.value)
+  const onAuthorChanged = e => setUserId(e.target.value)
 
   const onSavePostClicked = () => {
     if (title && content) {
-      dispatch(postAdded(title, content))
+      dispatch(postAdded(title, content, userId))
 
       setTitle('')
       setContent('')
     }
   }
+
+  const canSave = Boolean(title) && Boolean(content) && Boolean(userId)
+
+  const usersOptions = users.map(user =>(
+    <option key={user.id} value={user.id}>
+      {user.name}
+    </option>
+  ))
 
   return (
     <section>
@@ -32,6 +44,11 @@ const AddPostForm = () => {
           value={title}
           onChange={onTitleChanged}
         />
+        <label htmlFor="postAuthor">Author:</label>
+        <select id="postAuthor" value={userId} onChange={onAuthorChanged}>
+          <option value=""></option>
+          {usersOptions}
+        </select>
         <label htmlFor="postContent">Content:</label>
         <textarea
           id="postContent"
@@ -39,7 +56,7 @@ const AddPostForm = () => {
           value={content}
           onChange={onContentChanged}
         />
-        <button type="button" onClick={onSavePostClicked}>
+        <button type="button" onClick={onSavePostClicked} disabled={!canSave}>
           Save Post
         </button>
       </form>
--- src/features/posts/SinglePostPage.js
+++ src/features/posts/SinglePostPage.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
+import PostAuthor from './PostAuthor'
 
 const SinglePostPage = ({ match }) => {
   const { postId } = match.params
@@ -22,6 +23,7 @@ const SinglePostPage = ({ match }) => {
       <article className="post">
         <h2>{post.title}</h2>
         <p className="post-content">{post.content}</p>
+        <p><PostAuthor userId={post.user} /></p>
         <Link to={`/editPost/${post.id}`} className="button">
            Edit Post
         </Link>

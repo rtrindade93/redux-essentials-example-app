c5235ac1975e46f86ee21da02339596b387ec296
Ricardo Trindade <ricardopovoas.trindade@gmail.com>
2021-03-24 14:10:25 -0400

    improving render performance

--- src/features/users/UserPage.js
+++ src/features/users/UserPage.js
@@ -2,17 +2,14 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { selectUserById } from '../users/userSlice'
-import { selectAllPosts } from '../posts/postsSlice'
+import { selectPostsByUser } from '../posts/postsSlice'
 
 const UserPage = ({ match }) => {
   const { userId } = match.params
 
   const user = useSelector((state) => selectUserById(state, userId))
 
-  const postsForUser = useSelector((state) => {
-    const allPosts = selectAllPosts(state)
-    return allPosts.filter((post) => post.user === userId)
-  })
+  const postsForUser = useSelector((state) => selectPostsByUser(state, userId))
 
   const postTitles = postsForUser.map((post) => (
     <li key={post.id}>
